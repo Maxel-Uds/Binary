7d1463aeddae01864b362d15b9f5ef16866bf2e2
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-26 11:15:54 -0300

    Tratamento de exceções add

--- binary/Program.cs
+++ binary/Program.cs
@@ -5,37 +5,44 @@ namespace binary
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Escolha uma opção");
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("[1] De decimal para binário");
-            Console.WriteLine("[2] De binário para decimal");
-            Console.WriteLine("[0] Sair");
-            int n = int.Parse(Console.ReadLine());
-            Binary binary;
-            Console.Clear();
-
-            switch(n)
+            static void Main(string[] args)
             {
-                case 1:
-                Console.Write("Digite um número inteiro para transformar em binário: ");
-                int decimalNumber = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Escolha uma opção");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("[1] De decimal para binário");
+                Console.WriteLine("[2] De binário para decimal");
+                Console.WriteLine("[0] Sair");
+                int n = int.Parse(Console.ReadLine());
+                Binary binary;
+                Console.Clear();
 
-                binary = new Binary(decimalNumber);
-                Console.WriteLine(binary.DecimalToBinary());
-                break;
+                switch(n)
+                {
+                    case 1:
+                    Console.Write("Digite um número inteiro para transformar em binário: ");
+                    int decimalNumber = int.Parse(Console.ReadLine());
 
-                case 2:
-                Console.Write("Digite o número binário com espaços entre os digitos: ");
-                string binaryNumber = Console.ReadLine();
+                    binary = new Binary(decimalNumber);
+                    Console.WriteLine(binary.DecimalToBinary());
+                    break;
 
-                binary = new Binary(binaryNumber);
-                Console.WriteLine(binary.BinaryToDecimal());
-                break;
+                    case 2:
+                    Console.Write("Digite o número binário com espaços entre os digitos: ");
+                    string binaryNumber = Console.ReadLine();
 
-                default:
-                break;
+                    binary = new Binary(binaryNumber);
+                    Console.WriteLine(binary.BinaryToDecimal());
+                    break;
+
+                    default:
+                    break;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
